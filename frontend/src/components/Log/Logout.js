f54403746c1444dd6b56e054746a9b5c1d322746
FloFlo-L <flowfloxper@gmail.com>
2022-07-10 19:57:38 +0200

    logout

--- frontend/src/components/Log/Logout.js
+++ frontend/src/components/Log/Logout.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+import '../../style/components/NavBar.css';
 
 export default function Logout() {
+  function logout() {
+    localStorage.clear();
+    window.location('/connexion');
+  }
+
   return (
-    <div>Logout</div>
-  )
+    <li className="containerImgLog">
+      <NavLink onClick={logout} to="/connexion" className="NavIconBtn">
+        <FontAwesomeIcon
+          icon={faRightFromBracket}
+          size="2x"
+          className="iconLog"
+        ></FontAwesomeIcon>
+        <div className="btn btnLogNavBar">Déconnexion</div>
+        <div className="btn-small smallBtnLogNavBar">Déconnexion</div>
+      </NavLink>
+    </li>
+  );
 }
